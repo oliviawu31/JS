0b4aaf5950b816c4fccb284b30cf5ddb64d1fd1f
oliviawu <oliviawu0301@gmail.com>
2024-11-28 10:24:27 +0800

    js basic

---
+++ 20241128/test.php
@@ -0,0 +1,20 @@
+<?php
+$name = 'Olivia';
+echo("HI! $name");
+
+// array
+
+// index array
+$indexArr = [1,2,3];
+
+// Associative array
+$assocArr = [
+    's1' => 'amy',
+    's2' => 'bob',
+    's3' => 'cat',
+];
+
+
+
+
+?>
