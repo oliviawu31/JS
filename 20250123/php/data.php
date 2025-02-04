7406731f528f6903815ab1616665d93aec43ca8d
oliviawu <oliviawu0301@gmail.com>
2025-02-04 10:22:31 +0800

    中央氣象api 36小時最低溫最高溫

---
+++ 20250123/php/data.php
@@ -0,0 +1,15 @@
+<?php
+function dd($data)
+{
+    echo "<pre>";
+    print_r($data);
+    echo "</pre>";
+}
+$data = [
+    's1' => 'amy',
+    's2' => 'bob',
+    's3' => 'cat'
+];
+// dd($data);
+header('Access-Control-Allow-Origin: *');
+echo json_encode($data);
